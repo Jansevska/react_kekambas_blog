3b066988e61dcf349e49ca9031651896c637adea
Emili <ejanchevis@gmail.com>
2023-11-20 12:33:38 -1000

    imported axios(npm i axios), created lib folder and apiWrapper, created api.ts, modified PostView from fetch to async function

--- src/views/PostsView.tsx
+++ src/views/PostsView.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Post from "../components/Post"
 import PostType from '../types/post';
+import { getAllPosts } from '../lib/apiWrapper';
 
 
 
@@ -8,12 +9,22 @@ export default function PostsView() {
     
     const [posts, setPosts] = useState<PostType[]>([])
 
-    useEffect( () => {
-        fetch('https://jansevska-flaskblog-api.onrender.com/api/posts')
-        .then(res => res.json())
-        .then(data => setPosts(data))
-    }, [])
+    // fetch is the other way of getting data opposed to Axios(imported axios (npm i axios) -> added lib/apiWrapper.tx, /types/api.ts)
+    // useEffect( () => {
+    //     fetch('https://jansevska-flaskblog-api.onrender.com/api/posts')
+    //     .then(res => res.json())
+    //     .then(data => setPosts(data))
+    // }, [])
 
+    useEffect( () => {
+        async function fetchData() {
+            const response = await getAllPosts();
+            if (response.data){
+                setPosts(response.data);
+            }
+        };
+        fetchData()
+    }, [])
 
     return (
         <>
