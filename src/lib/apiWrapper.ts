8112c2a8f101a3d8cb02455d8089897638a83282
Emili <ejanchevis@gmail.com>
2023-11-20 13:39:33 -1000

    Add createNewUser and update SignUp component

--- src/lib/apiWrapper.ts
+++ src/lib/apiWrapper.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import PostType from '../types/post';
 import APIResponse from '../types/api';
+import UserType from '../types/auth';
 
 
 const base: string = 'https://jansevska-flaskblog-api.onrender.com/api'
 const postEndpoint: string = '/posts'
+const userEndpoint: string = '/users'
 
 
 const apiClientNoAuth = () => axios.create(
@@ -30,6 +32,25 @@ async function getAllPosts(): Promise<APIResponse<PostType[]>> {
     return {data, error}
 }
 
+
+async function createNewUser(newUserData:Partial<UserType>): Promise<APIResponse<UserType>> {
+    let data;
+    let error;
+    try {
+        const response = await apiClientNoAuth().post(userEndpoint, newUserData);
+        data = response.data
+    } catch(err) {
+        if (axios.isAxiosError(err)){
+            error = err.response?.data.error
+        } else {
+            error = 'Something went wrong'
+        }
+    }
+    return {data, error}
+}
+
+
 export {
     getAllPosts,
+    createNewUser,
 }
